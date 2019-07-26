36f82adf36c6130150fb7ad0b125599be284ece5
cloudymous <sayacloud@gmail.com>
2019-07-26 09:23:55 +0800

    I dont know

---
+++ omdb/js/script.js
@@ -0,0 +1,16 @@
+$('#search-button').on('click', function (){
+    
+    $.ajax({
+        url: 'http://omdbapi.com',
+        type: 'get',
+        dataType: 'json',
+        data: {
+            apikey : 'eb343a33',
+            s : $('#search-input').val()
+        },
+        success: function(result){
+            console.log(result)
+        }
+    });
+
+});
